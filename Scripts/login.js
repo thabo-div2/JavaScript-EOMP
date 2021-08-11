d1bb7aafcf2704848c2c76776511b1460209b563
Thabo Setsubi <thabosetsubi3@gmail.com>
2021-08-11 13:16:36 +0200

    Update

---
+++ Scripts/login.js
@@ -0,0 +1,34 @@
+let baseURL = "https://floating-sands-20442.herokuapp.com/auth";
+let accesstoken = window.localStorage.getItem("jwt-token");
+
+const form = document.querySelector("#form");
+
+function login(url) {
+	const username = document.querySelector("#username").value;
+	const password = document.querySelector("#password").value;
+	const body = {
+		username: username,
+		password: password,
+	};
+	fetch(url, {
+		method: "POST",
+		body: JSON.stringify(body),
+		headers: {
+			"Content-type": "application/json; charset=UTF-8",
+		},
+	})
+		.then((res) => res.json())
+		.then((json) => {
+			console.log(json);
+			myStorage = window.localStorage;
+			console.log(json["access_token"]);
+			myStorage.setItem("jwt-token", json["access_token"]);
+		});
+}
+
+function submitForm(event) {
+	event.preventDefault();
+	login(baseURL);
+}
+
+form.addEventListener("submit", submitForm);
--- Scripts/register.js
+++ Scripts/register.js
@@ -1,25 +1,24 @@
 // // fetching my api
 // const baseURL = "https://pacific-shelf-48886.herokuapp.com/user-registration/";
 
-// const fname = document.querySelector("#first_name").value;
-// const surname = document.querySelector("#last_name").value;
-// const address = document.querySelector("#address").value;
-// const email = document.querySelector("#email").value;
-// const username = document.querySelector("#username").value;
-// const password = document.querySelector("#password").value;
 // const form = document.querySelector("#form");
 // const log = document.getElementById("log");
 
-// const body = {
-// 	first_name: fname,
-// 	last_name: surname,
-// 	address: address,
-// 	email: email,
-// 	username: username,
-// 	password: password,
-// };
-
-// function UserRegistration(url) {
+// function userRegistration(url) {
+//     const fname = document.querySelector("#first_name").value;
+//     const surname = document.querySelector("#last_name").value;
+//     const address = document.querySelector("#address").value;
+//     const email = document.querySelector("#email").value;
+//     const username = document.querySelector("#username").value;
+//     const password = document.querySelector("#password").value;
+//     const body = {
+//         first_name: fname,
+//         last_name: surname,
+//         address: address,
+//         email: email,
+//         username: username,
+//         password: password,
+//     };
 // 	fetch(url, {
 // 		method: "POST",
 // 		body: JSON.stringify(body),
@@ -35,7 +34,7 @@
 // function submitForm(event) {
 // 	log.textContent = "You have successfully logged ";
 // 	event.preventDefault();
-// 	UserRegistration(baseURL);
+// 	userRegistration(baseURL);
 // }
 
 // form.addEventListener("submit", submitForm);
