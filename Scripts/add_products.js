51403393320db47d1a82104bb8e4b4bc0fa50f39
Thabo Setsubi <thabosetsubi3@gmail.com>
2021-08-13 14:49:18 +0200

    Update

--- Scripts/add_products.js
+++ Scripts/add_products.js
@@ -1,31 +1,43 @@
-let baseURL = "https://floating-sands-20442.herokuapp.com/create-products";
-let accesstoken = window.localStorage.getItem("jwt-token");
+const baseURL = "https://floating-sands-20442.herokuapp.com/create-products";
+const accesstoken = window.localStorage.getItem("jwt-token");
+const storage = window.localStorage;
 
 const add = document.querySelector("#add-form");
 
 function createProducts() {
-	const pname = document.querySelector("#name");
-	const price = document.querySelector("#price");
-	const desc = document.querySelector("#description");
-	const type = document.querySelector("#type");
-	const quantity = document.querySelector("#quantity");
+	const pname = document.querySelector("#name").value;
+	const price = document.querySelector("#price").value;
+	const desc = document.querySelector("#description").value;
+	const type = document.querySelector("#type").value;
+	const quantity = document.querySelector("#quantity").value;
 	const body = {
 		name: pname,
 		price: price,
 		description: desc,
 		type: type,
+		quantity: quantity,
+		total: price * quantity,
 	};
+	console.log(body);
+	console.log(window.localStorage.getItem("jwt-token"))
 	fetch(baseURL, {
 		method: "POST",
-		body: JSON.stringify(body),
+		body: JSON.stringify({
+			'name': pname,
+			'price': price,
+			'description': desc,
+			'type': type,
+			'quantity': quantity,
+			'total': (price * quantity),
+		}),
 		headers: {
-			"Content-type": "application/json; charset=UTF-8",
-			Authorization: `jwt ${window.localStorage.getItem("jwt-token")}`,
+			"Authorization": `jwt ${window.localStorage.getItem("jwt-token")}`,
+			"Content-type": "application/json",
 		},
+		mode: "no-cors"
 	})
-		.then((res) => res.json())
-		.then((res) => {
-			console.log(res);
+		.then((res) => res.json()).then((data) => {
+			console.log(data);
 		});
 }
 
--- Scripts/login.js
+++ Scripts/login.js
@@ -1,4 +1,4 @@
-let baseURL = "https://floating-sands-20442.herokuapp.com/auth";
+const baseURL = "https://floating-sands-20442.herokuapp.com/auth";
 const accesstoken = window.localStorage.getItem("jwt-token");
 
 const form = document.querySelector("#form");
@@ -17,44 +17,24 @@ function login(url) {
 			"Content-type": "application/json; charset=UTF-8",
 		},
 	})
-		.then((res) => res.json())
-		.then((json) => {
-			console.log(json);
-			myStorage = window.localStorage;
-			console.log(json["access_token"]);
-			myStorage.setItem("jwt-token", json["access_token"]);
+		.then((res) => res.json()).then((data) => {
+			console.log(data);
+			if (data['description'] == 'Invalid credentials') {
+				alert('Error not valid login in!')
+			}
+			else {
+				myStorage = window.localStorage
+				console.log(data['access_token'])
+				myStorage.setItem('jwt-token', data['access_token'])
+				window.location.href = './products.html'
+			}
 		});
 }
 
 function submitForm(event) {
 	event.preventDefault();
 	login(baseURL);
-	window.location.href = "/products.html";
 }
 
 form.addEventListener("submit", submitForm);
 
-function createProducts() {
-	const pname = document.querySelector("#name");
-	const price = document.querySelector("#price");
-	const desc = document.querySelector("#description");
-	const type = document.querySelector("#type");
-	const body = {
-		name: pname,
-		price: price,
-		description: desc,
-		type: type,
-	};
-	fetch("https://floating-sands-20442.herokuapp.com/create-products", {
-		method: "POST",
-		body: JSON.stringify(body),
-		headers: {
-			"Content-type": "application/json; charset=UTF-8",
-			Authorization: `jwt ${window.localStorage.getItem("jwt-token")}`,
-		},
-	})
-		.then((res) => res.json())
-		.then((json) => {
-			console.log(json);
-		});
-}
