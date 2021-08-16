27f4a4adca61a864541884af6c401153b20de957
Thabo Setsubi <thabosetsubi3@gmail.com>
2021-08-16 11:00:58 +0200

    Update

--- Scripts/add_products.js
+++ Scripts/add_products.js
@@ -4,46 +4,46 @@ const storage = window.localStorage;
 
 const add = document.querySelector("#add-form");
 
-function createProducts() {
-	const pname = document.querySelector("#name").value;
-	const price = document.querySelector("#price").value;
-	const desc = document.querySelector("#description").value;
-	const type = document.querySelector("#type").value;
-	const quantity = document.querySelector("#quantity").value;
-	const body = {
-		name: pname,
-		price: price,
-		description: desc,
-		type: type,
-		quantity: quantity,
-		total: price * quantity,
-	};
-	console.log(body);
-	console.log(window.localStorage.getItem("jwt-token"))
-	fetch(baseURL, {
-		method: "POST",
-		body: JSON.stringify({
-			'name': pname,
-			'price': price,
-			'description': desc,
-			'type': type,
-			'quantity': quantity,
-			'total': (price * quantity),
-		}),
-		headers: {
-			"Authorization": `jwt ${window.localStorage.getItem("jwt-token")}`,
-			"Content-type": "application/json",
-		},
-		mode: "no-cors"
-	})
-		.then((res) => res.json()).then((data) => {
-			console.log(data);
-		});
-}
+// function createProducts() {
+// 	const pname = document.querySelector("#name").value;
+// 	const price = document.querySelector("#price").value;
+// 	const desc = document.querySelector("#description").value;
+// 	const type = document.querySelector("#type").value;
+// 	const quantity = document.querySelector("#quantity").value;
+// 	const body = {
+// 		name: pname,
+// 		price: price,
+// 		description: desc,
+// 		type: type,
+// 		quantity: quantity,
+// 		total: price * quantity,
+// 	};
+// 	console.log(body);
+// 	console.log(window.localStorage.getItem("jwt-token"))
+// 	fetch(baseURL, {
+// 		method: "POST",
+// 		body: JSON.stringify({
+// 			'name': pname,
+// 			'price': price,
+// 			'description': desc,
+// 			'type': type,
+// 			'quantity': quantity,
+// 			'total': (price * quantity),
+// 		}),
+// 		headers: {
+// 			"Authorization": `jwt ${window.localStorage.getItem("jwt-token")}`,
+// 			"Content-type": "application/json",
+// 		},
+// 		mode: "no-cors"
+// 	})
+// 		.then((res) => res.json()).then((data) => {
+// 			console.log(data);
+// 		});
+// }
 
 function addProduct(event) {
 	event.preventDefault();
-	createProducts(baseURL);
+	// createProducts(baseURL);
 }
 
 add.addEventListener("submit", addProduct);
--- Scripts/products.js
+++ Scripts/products.js
@@ -12,17 +12,52 @@ function getProducts(url) {
 			let container = document.querySelector(".products");
 			container.innerHTML = "";
 			products.forEach((product) => {
-				container.innerHTML = `
+				container.innerHTML += `
                     <div class="product-container">
                         <h3>${product[1]}</h3>
                         <p>R${product[2]}</p>
                         <p>Description: <q>${product[3]}</q></p>
                         <p>Type: ${product[4]}</p>
+						<button id="myBtn">Update Product</button>
+
+						<div id="myModal" class="modal">
+							<div class="modal-content">
+								<span class="close">&times;</span>
+								<form></form>
+							</div>
+
+						</div>
                     </div>
                 `;
 			});
+			let modal = document.getElementById("myModal");
+
+			// Get the button that opens the modal
+			let btn = document.getElementById("myBtn");
+
+			// Get the <span> element that closes the modal
+			let span = document.getElementsByClassName("close")[0];
+
+			// When the user clicks on the button, open the modal
+			btn.onclick = function() {
+			modal.style.display = "block";
+			}
+
+			// When the user clicks on <span> (x), close the modal
+			span.onclick = function() {
+			modal.style.display = "none";
+			}
+
+			// When the user clicks anywhere outside of the modal, close it
+			window.onclick = function(event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+			}
 		});
 }
 
 getProducts(baseURL);
+
+
 
